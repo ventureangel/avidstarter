659098415410908e0c54ba35f1440a4fd7f33a9e
Sean Cuevo <seanpat09@gmail.com>
2012-07-03 14:15:25 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Avidstarter::Application.config.secret_token = '0ba7e9e1e575e11211164dd89b623ba8bb57deb5c2f53bc60093ebfbcfeeb5a97f1dad573895fc0da85f6943740616a3f47a296b895f5c3c7df200082bf2fc1c'
