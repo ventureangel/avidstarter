fd9fa9ef8ba9a0cc51b7452ad08e3d15396faac5
Sean Cuevo <seanpat09@gmail.com>
2012-08-06 15:31:44 -0400

    Added close studio function for last member leaving project

--- spec/models/membership_spec.rb
+++ spec/models/membership_spec.rb
@@ -5,7 +5,7 @@ describe Membership do
   it {should belong_to :project}
   it {should belong_to :account}
   it {should validate_presence_of :account}
-
+  let(:membership) {FactoryGirl.build(:membership)}
 
   it 'should have a valid factory' do
     Membership.stub(:notify_of_invitation)
@@ -15,13 +15,11 @@ describe Membership do
 
   it 'should call notify_of_invitation after create' do
     ProjectMembers.stub(:membership_invitation)
-    membership = FactoryGirl.build(:membership)
     membership.should_receive(:notify_of_invitation)
     membership.save
   end
 
   it 'should send out an email after inviting current account to project' do
-    membership = FactoryGirl.build(:membership)
     ProjectMembers.should_receive(:membership_invitation).with(membership).and_return(mail)
     membership.save
   end
@@ -32,4 +30,23 @@ describe Membership do
     membership.save
   end
 
+  it 'should delete the project if it is the last member to leave the project' do
+    membership.save
+    project_id = membership.project.id
+    membership.destroy
+    Project.where(id: project_id).empty?.should be true
+  end
+
+  it 'should not delete a project if it is not the last member' do
+    account = FactoryGirl.create(:account)
+    membership.save
+    project = membership.project
+    membership2 = project.memberships.build
+    membership2.account = account
+    membership2.pending = false
+    membership2.save
+    project_id = membership.project.id
+    membership.destroy
+    Project.where(id: project_id).empty?.should be false
+  end
 end
