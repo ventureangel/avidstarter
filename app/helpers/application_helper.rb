df5bc9bb09aad456dea7b06a302f8e3938d2fa47
Ivan Sidorjak <ivan.sidorjak@ventureangel.com>
2012-09-13 14:55:53 -0400

    Edit view for judges / competition
    Added class attribute to our link_to_add_fields & link_to_remove_fields
    helpers.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -27,15 +27,15 @@ module ApplicationHelper
     "http://www.linkedin.com/in/#{linkedin}"
   end
 
-  def link_to_add_fields(name, f, association)
+  def link_to_add_fields(name, f, association, c)
     new_object = f.object.class.reflect_on_association(association).klass.new
     fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
       render(association.to_s.singularize + "_fields", :f => builder)
     end
-    link_to_function(name, "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")")
+    link_to_function(name, "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")", :class => c)
   end
 
-  def link_to_remove_fields(name, f)
-    f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)")
+  def link_to_remove_fields(name, f, c)
+    f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)", :class => c)
   end
 end
