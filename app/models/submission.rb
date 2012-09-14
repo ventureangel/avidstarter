9f79b86adc78c62b7d32d6e8c84ee86c0c9d890a
Sean Cuevo <seanpat09@gmail.com>
2012-09-14 11:45:53 -0400

    Can now submit projects to competitions

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,6 +1,9 @@
 class Submission < ActiveRecord::Base
   belongs_to :competition
   belongs_to :project
-  attr_accessible :competition, :project
+  attr_accessible :competition_id, :project_id
+
+  validates :project_id, :uniqueness => {:scope => :competition_id}
+  validates :competition, :presence => true
 
 end
