3f2b94e97dc275937d3e6014b8d561f46db957e6
Ivan Sidorjak <ivan.sidorjak@ventureangel.com>
2012-11-01 12:52:13 -0400

    added action_mailer to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,11 @@ Avidstarter::Application.configure do
 
   # Enable threaded mode
   # config.threadsafe!
-
+  
+  # Disable delivery errors, bad email addresses will be ignored
+  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'haunted-fortress-8607.herokuapp.com' }
+  
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
